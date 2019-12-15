e187dbb3f67fc6e336436072e906775d38d4612e
chaninkou <michaelkoulove@yahoo.com>
2019-12-14 16:41:54 -0800

    Great review on stack, pushing and pop() in if statement, checking empty

--- src/valid/CheckValidParentheses.java
+++ src/valid/CheckValidParentheses.java
@@ -4,8 +4,11 @@ import java.util.Stack;
 
 public class CheckValidParentheses {
 	public boolean isValid(String s) {
+		// FILO
 		Stack<Character> stack = new Stack();
 
+		// Going through the whole string
+		// Only adding the right side to the stack
 		for (char w : s.toCharArray()) {
 			if (w == '(') {
 				stack.push(')');
@@ -13,13 +16,14 @@ public class CheckValidParentheses {
 				stack.push('}');
 			} else if (w == '[') {
 				stack.push(']');
-			} else if (stack.isEmpty() || stack.pop() != w) {
+			} else if (stack.isEmpty() || stack.pop() != w) { // stack.pop() will occur in if statement too
 				// if its empty, that means its odd number of element, if pop()
 				// does not equal to current index, then its not ()
 				return false;
 			}
 		}
 
+		// If stack is empty, then it will be true
 		return stack.isEmpty();
 	}
 }
