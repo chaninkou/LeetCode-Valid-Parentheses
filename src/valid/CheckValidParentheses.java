d3a3e929531624b2042c80c797e96ece674e6fc9
chaninkou <michaelkoulove@yahoo.com>
2020-01-24 21:43:34 -0800

    Great review on stack, using pop() in if statement and cool idea.

--- src/valid/CheckValidParentheses.java
+++ src/valid/CheckValidParentheses.java
@@ -17,13 +17,13 @@ public class CheckValidParentheses {
 			} else if (w == '[') {
 				stack.push(']');
 			} else if (stack.isEmpty() || stack.pop() != w) { // stack.pop() will occur in if statement too
-				// if its empty, that means its odd number of element, if pop()
-				// does not equal to current index, then its not ()
+				// if its empty, that means it was not [, ( or }
+				// Second statement will check if if its valid
 				return false;
 			}
 		}
 
-		// If stack is empty, then it will be true
+		// If stack is empty that means its valid parenthese
 		return stack.isEmpty();
 	}
 }
